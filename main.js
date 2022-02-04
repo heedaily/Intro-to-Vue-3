2b635453656aeb639507b48762f93947ea361de6
phjg81 <phjg81@gmail.com>
2022-02-05 02:05:40 +0900

    self study

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            // Solution
+            description: 'A warm Dobby\'s pair of socks.' 
+        }
+    }
+})
