630b5e624629366a689a1b6e016beb0a92e3c03c
phjg81 <phjg81@gmail.com>
2022-02-07 19:01:07 +0900

    L6 solution self

--- main.js
+++ main.js
@@ -8,8 +8,24 @@ const app = Vue.createApp({
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+            ],
+            hoverVariant: 'blue'
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        // solution
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        updateImage(variantImage, color) {
+            this.image = variantImage
+            this.hoverVariant = color
         }
     }
 })
